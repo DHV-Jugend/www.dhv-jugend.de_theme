68e42460c1b26c5e1721d96bb019a6d058f92e5a
Christoph Bessei <chris@bessei-it.eu>
2017-12-29 11:37:18 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Christoph Bessei
+ */
+
+add_action(
+    'wp_enqueue_scripts',
+    function () {
+        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    }
+);
