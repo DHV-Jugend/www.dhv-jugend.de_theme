d4825aee58dec805ec72f5c0cfb7e5de3048cdce
Christoph Bessei <chris@bessei-it.eu>
2017-12-29 11:41:35 +0100

    Initial commit

--- searchform.php
+++ searchform.php
@@ -1,18 +1,12 @@
-<?php
-/**
- * The searchform template file.
- * @package SongWriter
- * @since SongWriter 1.0.0
- */
-?>
 <form id="searchform" method="get" action="<?php echo esc_url(home_url('/')); ?>">
     <div class="searchform-wrapper"><input type="text" value="<?php echo esc_attr(get_search_query()); ?>" name="s"
                                            id="s" placeholder="<?php esc_attr_e('Search here...', 'songwriter'); ?>"/>
         <input type="submit" class="send icon_search" name="searchsubmit"
                value="<?php echo esc_attr('&amp;#x55;'); ?>"/>
         <div class="second_logo">
-            <a target="_blank" href="http://www.dhv.de"><img
-                        src="https://www.dhv-jugend.de/wp-content/uploads/2016/03/dhv_logo_jugend_blau.png"/></a>
+            <a target="_blank" href="https://www.dhv.de">
+                <img src="<?php echo trailingslashit(get_stylesheet_directory_uri()); ?>assets/img/dhv_logo_blau.png"/>
+            </a>
         </div>
     </div>
 </form>
